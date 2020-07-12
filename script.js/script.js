02f9fc62f81c929d1ddbd27601673cefbae88a4a
Deonna Loper <sweetvanloan@iMac.local>
2020-07-12 17:33:35 -0500

    AJAX bugs fixed

--- script.js/script.js
+++ script.js/script.js
@@ -1,9 +1,11 @@
+
 /*----- constants -----*/
 const $location = $("#location");
 const $main = $("#main");
-const $forcast = $("#forcast");
+// const $forcast = $("#forcast");
 const $rain = $("#rain");
 const $snow = $("#snow");
+const $input = $("input[type='text']");
 
 /*----- app's state (variables) -----*/
 let settings = {
@@ -15,28 +17,25 @@ let settings = {
         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
 		"x-rapidapi-key": "8e53b3da51msh6944abafc64ea95p1296a2jsnefa44b87d6ad"
 	}
-}
+};
 
-let weatherData;
+let weatherData, userInput;
 
-
-/*----- cached element references -----*/
 
 
 /*----- event listeners -----*/
 
+$("form").on("submit", handleGetData);;
 
 /*----- functions -----*/
 
-
-
-$('form').on("submit", handleGetData);
-
 function handleGetData(evt) {
     evt.preventDefault();
+    userInput = $input.val();
+   
     
     $.ajax(settings).done(function (response) {
-        url: "https://community-open-weather-map.p.rapidapi.com/find?type=link%252C%20accurate&units=imperial%252C%20metric&q=london"
+        url: "https://community-open-weather-map.p.rapidapi.com/find?type=link%252C%20accurate&units=imperial%252C%20metric&q=" + userInput
         console.log(response);
       (data) => {
        weatherData = data;
